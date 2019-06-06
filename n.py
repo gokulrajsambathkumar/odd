0f388d5aa82f694a381071c514e7a7927768b255
gokulrajsambathkumar <42170505+gokulrajsambathkumar@users.noreply.github.com>
2019-06-06 15:47:11 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,6 +1,6 @@
-b = int(input())
-sum = 0
-while(x > 0):
-    sum += b
-    b-= 1
-print(sum)
+a2,b2=map(int,input().split())
+n2=list(input().split())
+sum2=0
+for i in range(b2):
+    sum2=sum2+int(n2[i])
+print(sum2)
