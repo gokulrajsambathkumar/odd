477d4cb4954ebf612f14dac199be9ea32da93491
gokulrajsambathkumar <42170505+gokulrajsambathkumar@users.noreply.github.com>
2019-06-06 15:50:47 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,6 +1,10 @@
-a2,b2=map(int,input().split())
-n2=list(input().split())
-sum2=0
-for i in range(b2):
-    sum2=sum2+int(n2[i])
-print(sum2)
+ak1=input()
+ak1=ak1.split()
+bk1=int(ak1[1])
+ck1=input()
+ck1=ck1.split()
+dk1=0
+ck1=list(map(int,ck1))
+for i in range(0,bk1):
+    dk1=dk1+ck1[i]
+print(dk1)
