2fe97b9393d32f0d93a895a5160a64bee88bfebb
gokulrajsambathkumar <42170505+gokulrajsambathkumar@users.noreply.github.com>
2019-06-06 11:10:48 +0530

    Create vowelornot.py

---
+++ vowelornot.py
@@ -0,0 +1,8 @@
+ip=input()
+if(ip=="A" or ip=="a" or ip=="e" or ip=="E" or ip=="i" or ip=="I" or ip=="o" or ip=="O" or ip=="U" or ip=="u" ):
+  print("Vowel")
+elif(ip=="$"):
+    print("invalid")
+else:
+    print("consonant")
+  
