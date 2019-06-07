9c029934f312e971df92519a9854c3158ebb0334
gokulrajsambathkumar <42170505+gokulrajsambathkumar@users.noreply.github.com>
2019-06-07 11:13:11 +0530

    Update and rename even.py to even1.py

--- even.py
+++
@@ -1,6 +0,0 @@
-x,y=input().split()
-x=int(x)
-y=int(y)
-for i in range(x+1,y):
-    if(i%2==0):
-        print(i,end=' ')
---
+++ even1.py
@@ -0,0 +1,6 @@
+c,y=input().split()
+c=int(c)
+y=int(y)
+for i in range(c+1,y):
+    if(i%2==0):
+        print(i,end=' ')
